be22c9211b65e7e8f8878e9572156d6c71b8c417
Missa <makidon456@gmail.com>
2026-09-15 01:07:47 +0200

    Switch log format to JSON Lines; stop tracking log data file

--- key-value.py
+++ key-value.py
@@ -1,10 +1,14 @@
+import json
 
 class KVStore:
     def __init__(self):
         self.data = {}
 
-    def set(self, key, value):
+    def set(self, key, value=None):
         self.data[key] = value
+        record = {'key': key, 'value': value}
+        with open('/home/adun/PyCharmMiscProject/key-value/logs.jsonl', 'a', encoding='utf-8') as f:
+            f.write(json.dumps(record) + '\n')
 
     def get(self, key):
         return self.data.get(key)
