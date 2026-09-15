10eb203466cc3749229c8c58ff85a3b37338f8ba
Missa <makidon456@gmail.com>
2026-09-15 02:18:34 +0200

    add set and delete logs

--- key-value.py
+++ key-value.py
@@ -6,8 +6,8 @@ class KVStore:
 
     def set(self, key, value=None):
         self.data[key] = value
-        record = {'key': key, 'value': value}
-        with open('/home/adun/PyCharmMiscProject/key-value/logs.jsonl', 'a', encoding='utf-8') as f:
+        record = {'op':'set','key': key, 'value': value}
+        with open('./logs.jsonl', 'a', encoding='utf-8') as f:
             f.write(json.dumps(record) + '\n')
 
     def get(self, key):
@@ -15,9 +15,14 @@ class KVStore:
 
     def delete(self, key):
         self.data.pop(key, None)
+        record = {'op': 'delete', 'key': key}
+        with open('./logs.jsonl', 'a', encoding='utf-8') as f:
+            f.write(json.dumps(record) + '\n')
 
+#TEST
 store = KVStore()
 store.set('name', 'slime')
+store.set('name', 'skeleton')
 print(store.get('name'))
 store.delete('name')
 print(store.get('name'))
