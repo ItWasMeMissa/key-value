51bc634db21a6845971283e53abab53898d887d5
Missa <makidon456@gmail.com>
2026-09-16 02:51:42 +0200

    added ttl, litle rework self.data, get work in progres

--- key-value.py
+++ key-value.py
@@ -1,4 +1,4 @@
-import json
+import json, time
 
 class KVStore:
     def load(self):
@@ -10,7 +10,10 @@ class KVStore:
 
                     record = json.loads(line)
                     if record['op'] == 'set':
-                        self.data[record['key']] = record['value']
+                        self.data[record['key']] = {
+                            'value': record['value'],
+                            'expire_at': record['expire_at']
+                        }
 
                     if record['op'] == 'delete':
                         self.data.pop(record['key'], None)
@@ -21,18 +24,28 @@ class KVStore:
         self.data = {}
         self.load()
 
-
     def _append_log(self, record): #test name?
         with open('./logs.jsonl', 'a', encoding='utf-8') as f:
             f.write(json.dumps(record) + '\n')
 
-    def set(self, key, value=None):
-        self.data[key] = value
-        record = {'op':'set','key': key, 'value': value}
+    def set(self, key, value=None, ttl=None):
+        if ttl is not None:
+            ttl += time.time()
+            self.data[key] = {'value': value, 'expire_at': ttl}
+        else:
+            self.data[key] = {'value': value, 'expire_at': ttl}
+        # how note record time? Days Hours Sec "%H:%M:%S in cli?"
+        record = {'op':'set','key': key, 'value': value, 'expire_at': ttl}
         self._append_log(record)
 
     def get(self, key):
-        return self.data.get(key)
+        if not self.data.get(key): #None == False
+            return 'not found'
+        else:
+            if self.data[key]['expire_at'] < time.time():
+                return self.data[key]
+            else:
+                self.delete(key)
 
     def delete(self, key):
         self.data.pop(key, None)
@@ -41,5 +54,7 @@ class KVStore:
 
 #TEST
 # store = KVStore()
-# store.load()
+#
+# print(store.get('test'))
+#
 # print(store.data)
